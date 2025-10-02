11164f581ba0c94b4e64802309aa74557fb23fbd
desguerra728 <desguerra728@phm.k12.in.us>
2025-10-02 08:45:28 -0400

    Update dataAnalysis.py

--- .github/dataAnalysis.py
+++ .github/dataAnalysis.py
@@ -1,23 +1,10 @@
 import pandas as pd
 import random
 
-names = []
-f_names = []
-l_names = []
-
-for i in range(30):
-    names.append(f"{random.choice(f_names)} {random.choice(l_names)}")
-    
 data = {
-    "Name": names,
-    "Grade": [random.randint(9,12) for _ in names],
-    "Hrs Spent Reading": [random.randint(0,60) for _ in names],
-    "Enjoy English Class":,
-    "Enjoy Reading ":,
-    "Enjoy Writing":,
+ f_names = [],
+ l_names = [],
+ grade = [],
+
+
 }
-
-pennData = pd.DataFrame(data)
-print(pennData)
-
-pennData.to_csv("pennData.csv", index=False)
