c0cedd2369000948b848af391910f46242d9479a
desguerra728 <desguerra728@phm.k12.in.us>
2025-10-02 09:24:58 -0400

    s

--- .github/dataAnalysis.py
+++ .github/dataAnalysis.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import random
 
-f_names = ['Noah', 'Alexis', 'Katerine', 'Van', 'Aiden']
-l_names = ['Bradberry', 'Luebke', 'Kedrychowicz', 'Latson', 'Girton']
-grades = [10, 11]
+f_names = ['Noah', 'Alexis', 'Katerina', 'Van', 'Aiden', 'Kyan', 'Ben', 'Martin', 'Roman']
+l_names = ['Bradberry', 'Luebke', 'Kedrychowicz', 'Latson', 'Girton', 'Fogarty', 'Martin', 'Williams']
+grades = [10, 11, 12]
 names = []
-hours = ['<=1', '2', '3', '3+']
+hours = ['<1', '2', '3', '3+']
 
 
 for i in range(30):
@@ -17,9 +17,9 @@ junk_data = {
  'Name': names,
  'Grade': [random.choice(grades) for _ in names],
  'Hrs reading per week': [random.choice(hours) for _ in names],
- 'Language Arts Class': [],
- 'Reading': [],
- 'Writing': [],
+ 'Language Arts Class': [random.randint(0,5) for _ in names],
+ 'Reading': [random.randint(0,5) for _ in names],
+ 'Writing': [random.randint(0,5) for _ in names],
 }
 
 data = pd.DataFrame(junk_data)
